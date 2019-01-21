113596a27138565295de729d1098dbd95715e7b4
ThiruDevel <46588251+ThiruDevel@users.noreply.github.com>
2019-01-21 12:49:56 +0530

    Add files via upload

--- truffle.js
+++ truffle.js
@@ -1,4 +1,8 @@
 var HDWalletProvider = require('truffle-hdwallet-provider');
+var Web3 = require('web3');
+var api_url = "https://ropsten.infura.io/HFGGXWXJ7PU5UXI9IHKJ5RIK7YWWC1Y9RH";
+var web3 = new Web3(new Web3.providers.HttpProvider(api_url));
+
 var MNEMONIC = 'follow merit panic insane stomach cabin aerobic prosper romance acquire aisle horror';
 module.exports = {
 networks : {
@@ -12,7 +16,8 @@ networks : {
           return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/HFGGXWXJ7PU5UXI9IHKJ5RIK7YWWC1Y9RH")
         },
         network_id: 3,
-        gas: 4706206      //make sure this gas allocation isn't over 4M, which is the max
+      //  gas: 30000000      //make sure this gas allocation isn't over 4M, which is the max
+      //  gas: web3.eth.estimateGas({from:"0x85963D5750A0Cf968fE9417E3A742B8b7a9718B8"})
       }
 },
 solc: {
